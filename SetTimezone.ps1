93546e80d43b32ca2734144a9e08ca0f6bb58a0f
watervsanchor <100880673+watervsanchor@users.noreply.github.com>
2023-07-24 16:10:04 -0600

    Add files via upload

---
+++ SetTimezone.ps1
@@ -0,0 +1,3 @@
+#Set desired timezone. Get available options using "Get-Timezone -ListAvailable"
+$command = {Set-TimeZone -Id "Timezone"}
+Invoke-Command -ComputerName LON-W-021 -ScriptBlock $command
