e6d0758c34e6ec985ad54d6df8b776c900c5151b
watervsanchor <100880673+watervsanchor@users.noreply.github.com>
2023-07-24 16:16:12 -0600

    Add files via upload

---
+++ PagefileSet.ps1
@@ -0,0 +1,9 @@
+# PowerShell Script to set the size of pagefile.sys
+
+$computersys = Get-WmiObject Win32_ComputerSystem -EnableAllPrivileges;
+$computersys.AutomaticManagedPagefile = $False;
+$computersys.Put();
+$pagefile = Get-WmiObject -Query "Select * From Win32_PageFileSetting Where Name like '%pagefile.sys'";
+$pagefile.InitialSize = 4096;
+$pagefile.MaximumSize = 8192;
+$pagefile.Put();
